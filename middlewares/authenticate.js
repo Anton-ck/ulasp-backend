3b9000c9a16b91d25be661fdf9fee99f0386b1b3
Irina Iskra <iskrairina.a@gmail.com>
2023-10-19 18:01:33 +0300

    logout/current

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 import Admin from "../models/admin.js";
-
+import User from "../models/userModel.js";
 import HttpError from "../helpers/HttpError.js";
 
 dotenv.config();
@@ -26,6 +26,28 @@ export const authenticateAdmin = async (req, res, next) => {
     }
 
     req.admin = admin;
+    next();
+  } catch (error) {
+    next(HttpError(401));
+  }
+};
+export const authenticatUser = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, accessToken] = authorization.split(" ");
+
+  if (bearer !== "Bearer") {
+    next(HttpError(401));
+    return;
+  }
+
+  try {
+    const { id } = jwt.verify(accessToken, ACCESS_SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user || !user.accessToken || user.accessToken !== accessToken) {
+      next(HttpError(401));
+    }
+
+    req.user = user;
     next();
   } catch (error) {
     next(HttpError(401));
