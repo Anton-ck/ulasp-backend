6e5eb70e154132130e3cd669fd2de39be303d266
Irina Iskra <iskrairina.a@gmail.com>
2024-02-12 21:31:46 +0200

    fix schema user

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -27,7 +27,7 @@ export const createCommonUserSchema = Joi.object({
   userFop: Joi.string().default(true),
   telNumber: Joi.string().pattern(phoneNumberUaRegexp).required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  contactFace: Joi.string().min(5).max(30),
+  contactFace: Joi.string().min(3).max(30),
   contactFaceTaxCode: Joi.string().min(8).max(10).pattern(onlyNumberRegexp),
   contactFaceTelNumber: Joi.string().pattern(phoneNumberUaRegexp).required(),
   contactFaceEmail: Joi.string().pattern(emailRegexp).required(),
@@ -50,8 +50,7 @@ export const createFopUserSchema = createCommonUserSchema.keys({
 export const createCompanyUserSchema = createCommonUserSchema.keys({
   name: Joi.string().min(5).max(30).pattern(nameCompanyRegexp).required(),
   taxCode: Joi.string().min(8).max(10).pattern(onlyNumberRegexp),
-  // lastPay: Joi.string().pattern(regularDateRegexp).required(),
-});
+  });
 
 export const playListUserSchema = Joi.object({
   playListName: Joi.string().required().empty(false).min(1).max(30),
