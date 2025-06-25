aff6eacac7b39b041dafa5e93220cb229b419d07
Anton Nikolaenko <anton.ck.ua@gmail.com>
2025-06-25 15:14:30 +0300

    isValidIMG

--- helpers/autoPicureForTrack.js
+++ helpers/autoPicureForTrack.js
@@ -1,28 +1,28 @@
-import albumArt from "album-art";
-import getId3Tags from "./id3Tags.js";
-import { resizeTrackCover } from "./resizePics.js";
+import albumArt from 'album-art';
+import getId3Tags from './id3Tags.js';
+import { resizeTrackCover } from './resizePics.js';
 const autoPictureForTrack = async (artist, trackName, trackURL) => {
   const metadata = await getId3Tags(trackURL);
 
   const { album } = metadata?.common;
 
-  console.log("metadata", metadata);
+  // console.log("metadata", metadata);
 
   const trackPicture = await albumArt(artist, {
     album,
 
-    size: "large",
+    size: 'large',
   });
-  console.log("artist", artist);
-  console.log("album", album);
-  console.log("trackPicture", trackPicture);
+  console.log('artist', artist);
+  console.log('album', album);
+  console.log('trackPicture', trackPicture);
 
   if (!trackPicture) {
     return;
   }
   const resizeTrackCoverURL = await resizeTrackCover(
     trackPicture,
-    "trackCover"
+    'trackCover',
   );
 
   return resizeTrackCoverURL;
--- middlewares/isValidImage.js
+++ middlewares/isValidImage.js
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv';
 import defaultTrackCover from '../common/resources/defaultCovers.js';
 import Track from '../models/trackModel.js';
+import PlayList from '../models/playlistModel.js';
+
+import autoPictureForTrack from '../helpers/autoPicureForTrack.js';
+
+import path from 'path';
 
 dotenv.config();
 
@@ -16,32 +21,74 @@ const isImageUrlValid = async (url) => {
   }
 };
 
+// const isValidImageUrl = async (req, res, next) => {
+//   const bulkOps = [];
+
+//   try {
+//     const songs = await Track.find({
+//       trackPictureURL: { $exists: true, $ne: defaultTrackCover },
+//     }).lean();
+
+//     await Promise.all(
+//       songs.map(async ({ _id, trackPictureURL }) => {
+//         const isValid = await isImageUrlValid(
+//           `${BASE_URL}:${PORT}/${trackPictureURL}`,
+//         );
+//         if (!isValid) {
+//           bulkOps.push({
+//             updateOne: {
+//               filter: { _id },
+//               update: { $set: { trackPictureURL: defaultTrackCover } },
+//             },
+//           });
+//         }
+//       }),
+//     );
+//     if (bulkOps.length > 0) {
+//       await Track.bulkWrite(bulkOps);
+//     }
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 const isValidImageUrl = async (req, res, next) => {
   const bulkOps = [];
 
+  const publicDir = path.resolve('public/');
+
+  const { id } = req.params;
+
   try {
+    const playlist = await PlayList.findById(id);
+
     const songs = await Track.find({
-      trackPictureURL: { $exists: true, $ne: defaultTrackCover },
+      _id: playlist.trackList,
+      trackPictureURL: defaultTrackCover,
     }).lean();
 
     await Promise.all(
-      songs.map(async ({ _id, trackPictureURL }) => {
-        const isValid = await isImageUrlValid(
-          `${BASE_URL}:${PORT}/${trackPictureURL}`,
-        );
-        if (!isValid) {
+      songs.map(async ({ _id, artist, trackName, trackURL }) => {
+        const URL = path.join(publicDir, trackURL);
+
+        const result = await autoPictureForTrack(artist, trackName, URL);
+
+        if (result) {
           bulkOps.push({
             updateOne: {
               filter: { _id },
-              update: { $set: { trackPictureURL: defaultTrackCover } },
+              update: { $set: { trackPictureURL: result } },
             },
           });
         }
       }),
     );
+
     if (bulkOps.length > 0) {
       await Track.bulkWrite(bulkOps);
     }
+
     next();
   } catch (error) {
     next(error);
